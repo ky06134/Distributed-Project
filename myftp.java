535f8726ff295311e5e04a4b0d8ce533bdb662d3
ky06134 <ky06134@uga.edu>
2024-02-04 19:45:43 -0500

    edited client and server

--- myftp.java
+++ myftp.java
@@ -4,9 +4,13 @@ import java.net.*;
 // Client class 
 class myftp { 
     
-    public static void main(String[] args) { 
+    public static void main(String[] args) throws IOException{ 
         
-        Socket sr = new Socket("localhost", 4333);
+        Socket client = new Socket("localhost", 4333);
+       
+        PrintWriter text = new PrintWriter(client.getOutputStream());
+        text.println("hello");
+        text.flush();
         
     }
 }
--- myftpserver.java
+++ myftpserver.java
@@ -1,12 +1,25 @@
 import java.io.*;
 import java.net.*;
+import java.util.*;
 
 public class myftpserver {
     public static void main(String[]args) throws Exception {
 
-        ServerSocket s = new ServerSocket(4333);
-        Socket sr=s.accept();
+        ServerSocket server = new ServerSocket(4333);
+        System.out.println("server is now online");
 
+        Socket s=server.accept(); //waits for connection from client
+
+        System.out.println("Client Connected");
+        System.out.println("Enter Command: ");
+        
+        InputStreamReader in = new InputStreamReader(s.getInputStream());
+        BufferedReader bf = new BufferedReader(in);
+
+        String str = bf.readLine();
+        System.out.println("client : " + str);
+
+        System.exit(0);
     
     }
 }
