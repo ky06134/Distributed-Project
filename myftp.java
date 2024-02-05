a5a5ddf9bd30c69e2fdf4382aa20f66f047a7c74
ky06134 <ky06134@uga.edu>
2024-02-04 19:06:57 -0500

    updates to name and file content

---
+++ myftp.java
@@ -0,0 +1,12 @@
+import java.io.*; 
+import java.net.*; 
+  
+// Client class 
+class myftp { 
+    
+    public static void main(String[] args) { 
+        
+        Socket sr = new Socket("localhost", 4333);
+        
+    }
+}
---
+++ myftpserver.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.net.*;
+
+public class myftpserver {
+    public static void main(String[]args) throws Exception {
+
+        ServerSocket s = new ServerSocket(4333);
+        Socket sr=s.accept();
+
+    
+    }
+}
